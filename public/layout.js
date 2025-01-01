33bc2ba855edab86ef8090b0cb4c3e39765b0df4
Alex Pooley <zuedev@gmail.com>
2025-01-01 05:24:11 +0000

    enhance tab animation with dynamic emojis and titles

--- public/layout.js
+++ public/layout.js
@@ -1,19 +1,29 @@
-// animated favicon!
+// animated tab
 setInterval(() => {
-  const animation = ["🌍", "🌏", "🌎"];
-  window.animationStep = (window.animationStep || 0) % animation.length;
+  const animation = [
+    {
+      emoji: "🌍",
+      title: "AROUND the world",
+    },
+    {
+      emoji: "🌏",
+      title: "around THE world",
+    },
+    {
+      emoji: "🌎",
+      title: "around the WORLD",
+    },
+  ];
+
+  window.tabAnimationStep = (window.tabAnimationStep || 0) % animation.length;
 
   document.querySelector(
     "link[rel='icon']"
   ).href = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${
-    animation[window.animationStep++]
+    animation[window.tabAnimationStep].emoji
   }</text></svg>`;
-}, 1000);
 
-// randomise the title on an interval
-setInterval(() => {
-  const titles = ["Hi there!", "Welcome to my site!", "Have a nice day!"];
-  window.titleStep = (window.titleStep || 0) % titles.length;
+  document.title = animation[window.tabAnimationStep].title;
 
-  document.title = titles[window.titleStep++];
+  window.tabAnimationStep++;
 }, 1000);
